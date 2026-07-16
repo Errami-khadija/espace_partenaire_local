2ed3bb32e90caffef815dfd506b7f0496dc0c51a
Errami-khadija <khadiijaerrami2021@gmail.com>
2026-07-16 21:15:26 +0200

    feat: improve announcement form with validation and preview

--- src/announcements/announcements.ts
+++ src/announcements/announcements.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { AnnouncementForm } from './announcement-form/announcement-form';
 
 @Component({
   selector: 'app-announcements',
   standalone: true,
-  imports: [],
+  imports: [AnnouncementForm],
   templateUrl: './announcements.html',
   styleUrl: './announcements.css',
 })
-export class Announcements {}
+export class Announcements {
+
+   showForm = false;
+
+  openForm() {
+    this.showForm = true;
+  }
+
+  closeForm() {
+    this.showForm = false;
+  }
+}
